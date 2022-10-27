5eb2bc818baa7d19e6446542c0e29aa6df4b2409
Francesca-Barcella <pollon.fra@gmail>
2022-10-27 16:08:51 +0200

    VS- v-for

---
+++ assets/js/app.js
@@ -0,0 +1,43 @@
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            message: 'messaggio di prova',
+            description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit empore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+
+            videogames: [
+                {
+                    image: './assets/img/01.webp',
+                    title: 'Marvel\'s Spiderman Miles Morale',
+                    text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+                    altText: 'Spiderman'
+                },
+                {
+                    image: './assets/img/02.webp',
+                    title: 'Ratchet & Clank: Rift Apart',
+                    text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+                    altText: 'Ratchet & Clank'
+                },
+                {
+                    image: './assets/img/03.webp',
+                    title: 'Fortnite',
+                    text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+                    altText: 'Fortnite'
+                },
+                {
+                    image: './assets/img/04.webp',
+                    title: 'Stray',
+                    text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+                    altText: 'Stray'
+                },
+                {
+                    image: './assets/img/05.webp',
+                    title: "Marvel's Avengers",
+                    text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+                    altText: 'Avengers'
+                }
+            ]
+        }
+    }
+}).mount('#app')
